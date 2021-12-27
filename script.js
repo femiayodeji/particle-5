8910ca65a0eb467f981698b0a94324839e4a61a5
Femi Ayodeji <femidotayodeji@gmail.com>
2021-12-27 18:32:05 +0100

    Add Canvas Click

--- script.js
+++ script.js
@@ -9,7 +9,22 @@ window.addEventListener("resize", function(){
     canvas.height = window.innerHeight;
 })
 
-context.fillStyle = "blue";
-context.beginPath ();
-context.arc(100,100, 50, 0, Math.PI * 2);
-context.fill();
+const mouse = {
+    x : null,
+    y: null
+}
+
+canvas.addEventListener("click", function(event){
+    mouse.x = event.x;
+    mouse.y = event.y;
+    drawCircle();
+});
+
+function drawCircle(){
+    context.fillStyle = "blue";
+    context.beginPath ();
+    context.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2);
+    context.fill();    
+}
+
+drawCircle();
