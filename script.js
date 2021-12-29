df37c56ebe948d2e21d0dcae80a16d39eaedc1b8
Femi Ayodeji <femidotayodeji@gmail.com>
2021-12-29 21:26:59 +0100

    Add Multiple Particles

--- script.js
+++ script.js
@@ -10,9 +10,11 @@ window.addEventListener("resize", function(){
 })
 
 const mouse = {
-    x : 100,
-    y: 100
+    x : undefined,
+    y: undefined
 }
+
+const particles = [];
 
 canvas.addEventListener("click", function(event){
     mouse.x = event.x;
@@ -22,14 +24,51 @@ canvas.addEventListener("click", function(event){
 canvas.addEventListener("mousemove", function(event){
     mouse.x = event.x;
     mouse.y = event.y;
-    drawCircle();
 })
 
-function drawCircle(){
-    context.fillStyle = "blue";
-    context.beginPath ();
-    context.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2);
-    context.fill();    
+class Particle{
+    constructor(){
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 5 + 1;
+        this.speedx = Math.random() * 3 - 1.5;
+        this.speedy = Math.random() * 3 - 1.5;
+    }
+
+    update(){
+        this.x += this.speedx;
+        this.y += this.speedy;
+    }
+
+    draw(){
+        context.fillStyle = "blue";
+        context.beginPath ();
+        context.arc(this.x, this.y, 50, 0, Math.PI * 2);
+        context.fill();        
+    }
 }
 
-drawCircle();
+function init(){
+    for(let i = 0; i < 100; i++){
+         particles.push(new Particle())
+    }
+}
+
+init();
+
+function handleParticles(){
+    for(let i = 0; i < 100; i++){
+        let particle = particles[i];
+        particle.draw();
+        particle.update();
+   }
+}
+
+
+function animate(){
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    handleParticles();
+    requestAnimationFrame(animate);
+}
+
+animate();
